19fe9932ceab6ba0c3d32cecc97e9f0f61b6d2dc
Tim Brandle <tbrandle@ritchiebros.com>
2024-03-08 12:37:25 -0700

    add api hook

---
+++ src/hooks/useApi.tsx
@@ -0,0 +1,47 @@
+import { ErrorInfo, useState } from "react";
+import { Resume } from "../types/resumeTypes";
+
+interface FetchArgs <T>{
+  method: string;
+  url?: string;
+  body?: T;
+}
+
+const useApi = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<unknown>();
+
+  const baseUrl = "http://localhost:8080/resumes";
+
+  const fetchResume = async <T,>({ method, url, body }: FetchArgs<T>) => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(`${baseUrl}${url ? `/${url}` : ''}`, {
+        method,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+      const responseJson = await response.json();
+      setIsLoading(false);
+      return responseJson;
+    } catch (error) {
+      setIsLoading(false);
+      setError(error);
+    }
+  };
+
+  const api = {
+    get: async (id?: string) => fetchResume({ method: "GET", url: id }),
+    post: async (body: Resume) => fetchResume({ method: "POST", body }),
+    patch: async (id: string, body: Resume) =>
+      fetchResume({ method: "PATCH", url: id, body }),
+    delete: async (id: string) => fetchResume({ method: "DELETE", url: id }),
+  };
+
+  return { api, error, isLoading };
+};
+
+export default useApi;
