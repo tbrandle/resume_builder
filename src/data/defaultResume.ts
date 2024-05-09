4a416eb8811cf3723b3ad9bbc76e135c9a3f302a
Tim Brandle <tbrandle53@gmail.com>
2024-05-09 15:39:33 -0600

    start to hook up backend

--- src/data/defaultResume.ts
+++ src/data/defaultResume.ts
@@ -1,7 +1,7 @@
 import { uniqueId } from "lodash";
 import { Resume, Skill, SkillLevel } from "../types/resumeTypes";
 
-export const defaultResume: Resume = {
+const masterResume: Resume = {
   resume_title: "Master",
   personal_details: {
     first_name: "Tim",
@@ -572,6 +572,195 @@ export const defaultResume: Resume = {
           label: "state",
           type: "text",
           value: "CO",
+        },
+      ],
+    },
+  ],
+};
+
+
+export const defaultResume: Resume = {
+  resume_title: "",
+  personal_details: {
+    first_name: "",
+    last_name: "",
+    job_title: "",
+    professional_summary: "",
+    phone_number: "",
+    email: "",
+    fields: [
+      {
+        id: uniqueId(),
+        label: "first_name",
+        type: "text",
+        value: "",
+      },
+      {
+        id: uniqueId(),
+        label: "last_name",
+        type: "text",
+        value: "",
+      },
+      {
+        id: uniqueId(),
+        label: "job_title",
+        type: "text",
+        value: "",
+      },
+      {
+        id: uniqueId(),
+        label: "professional_summary",
+        type: "html",
+        value: "",
+      },
+      {
+        id: uniqueId(),
+        label: "phone_number",
+        type: "phone",
+        value: "",
+      },
+      {
+        id: uniqueId(),
+        label: "email",
+        type: "email",
+        value: "",
+      },
+    ],
+  },
+  social_media: {
+    linked_in: "",
+    github: "",
+    portfolio: "",
+    fields: [
+      {
+        id: uniqueId(),
+        label: "linked_in",
+        type: "text",
+        value: "",
+      },
+      {
+        id: uniqueId(),
+        label: "github",
+        type: "text",
+        value: "",
+      },
+      {
+        id: uniqueId(),
+        label: "portfolio",
+        type: "text",
+        value: "",
+      },
+    ],
+  },
+  skills: [
+    {
+      skill: "",
+      skill_level: "",
+      id: uniqueId(),
+      fields: [
+        {
+          id: uniqueId(),
+          label: "skill",
+          type: "text",
+          value: "",
+        },
+        {
+          id: uniqueId(),
+          label: "skill_level",
+          type: "select",
+          value: "",
+          options: ["Novice", "Beginner", "Skillful", "Experienced", "Expert"],
+        },
+      ],
+    },
+  ],
+  employment_history: [
+    {
+      job_title: "",
+      employer: "",
+      city: "",
+      state: "",
+      date_start: "",
+      date_end: "",
+      description: "",
+      id: uniqueId(),
+      fields: [
+        {
+          id: uniqueId(),
+          label: "job_title",
+          type: "text",
+          value: "",
+        },
+        {
+          id: uniqueId(),
+          label: "employer",
+          type: "text",
+          value: "",
+        },
+        {
+          id: uniqueId(),
+          label: "city",
+          type: "text",
+          value: "",
+        },
+        {
+          id: uniqueId(),
+          label: "state",
+          type: "text",
+          value: "",
+        },
+        {
+          id: uniqueId(),
+          label: "date_start",
+          type: "text",
+          value: "",
+        },
+        {
+          id: uniqueId(),
+          label: "date_end",
+          type: "text",
+          value: "",
+        },
+        {
+          id: uniqueId(),
+          label: "description",
+          type: "html",
+          value: "",
+        },
+      ],
+    },
+  ],
+  education: [
+    {
+      school: "",
+      degree: "",
+      city: "",
+      state: "",
+      id: uniqueId(),
+      fields: [
+        {
+          id: uniqueId(),
+          label: "school",
+          type: "text",
+          value: "",
+        },
+        {
+          id: uniqueId(),
+          label: "degree",
+          type: "text",
+          value: "",
+        },
+        {
+          id: uniqueId(),
+          label: "city",
+          type: "text",
+          value: "",
+        },
+        {
+          id: uniqueId(),
+          label: "state",
+          type: "text",
+          value: "",
         },
       ],
     },
